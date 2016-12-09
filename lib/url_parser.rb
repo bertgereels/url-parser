06818a28519fb8a237f5155eefe9ae7dc480bfbc
Bert Gereels <Bert.gereels@gmail.com>
2016-12-09 15:17:58 +0100

    added more tests

--- lib/url_parser.rb
+++ lib/url_parser.rb
@@ -1,10 +1,29 @@
 
-
 class UrlParser
 
+  private
+  attr_accessor :url
 
+  public
+  def initialize url    #initialize must be in small letters
+    @url = url
+  end
 
+  def host
+    (split "://").last
+  end
 
+  def scheme
+    (split "://").first
+  end
 
+  def path
+    (host.split "/").last
+  end
+
+  private
+  def split delimiter
+    url.split delimiter
+  end
 
 end
--- test/url_parser.rb
+++ test/url_parser.rb
@@ -4,8 +4,23 @@ require 'url_parser'
 class UrlParserTest < Minitest::Test
 
   def test_hostname
-    url = UrlParser.new("http://reddit.com")
-    assert_equals("reddit.com", url.host)
-
+    url = UrlParser.new "http://reddit.com"
+    assert_equal "reddit.com", url.host
   end
+
+  def test_scheme
+    url = UrlParser.new "https://google.be"
+    assert_equal "https", url.scheme
+  end
+
+  def test_path
+    url = UrlParser.new "http://youtube.com/caseyneistat"
+    assert_equal "caseyneistat", url.path
+  end
+
+  def test_long_path
+    url = UrlParser.new "https://www.youtube.com/user/caseyneistat/about"
+    assert_equal "caseyneistat", url.path
+  end
+
 end
